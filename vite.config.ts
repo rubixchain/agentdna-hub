47c86448046474a069693aec3164e28e39d873e7
Arnab Ghose <arnab.ghose128@gmail.com>
2025-12-11 07:12:09 -0800

    add environment variables for vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   plugins: [react()],
 
   server: {
-    port: 4000,      // change port
+    port: parseInt(process.env.HUB_SERVER_PORT, 10) || 4000,
     host: true,      // allow access from network (0.0.0.0)
-    strictPort: true // prevents auto-switching if port is busy
+    strictPort: true, // prevents auto-switching if port is busy
+    allowedHosts: [process.env.HUB_ALLOWED_HOSTS]
   }
 })
