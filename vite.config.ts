5f42bd4238a632ee1f9e264a060b1cb00c1babd8
aditysoni <adisoni1017@gmail.com>
2025-12-11 21:30:19 +0530

    updated dashboard redirection link and fixed bug in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const SERVER_PORT = process.env.HUB_SERVER_PORT || "3000" 
+const HUB_ALLOWED_HOSTS = process.env.HUB_ALLOWED_HOSTS || "localhost"
 export default defineConfig({
   plugins: [react()],
 
   server: {
-    port: parseInt(process.env.HUB_SERVER_PORT, 10) || 4000,
+    port: parseInt(SERVER_PORT, 10) || 4000,
     host: true,      // allow access from network (0.0.0.0)
     strictPort: true, // prevents auto-switching if port is busy
-    allowedHosts: [process.env.HUB_ALLOWED_HOSTS]
+    allowedHosts: [HUB_ALLOWED_HOSTS]
   }
 })
